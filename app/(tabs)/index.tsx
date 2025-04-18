a407300c192ca682976628e2df9a7ab981530669
Dilip Prajapati <dilipp11082004@gmail.com>
2025-04-18 15:10:46 +0530

    RequestCard added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { Bell, Heart, Users, Award } from "lucide-react-native";
 import { useAuth } from "@/hooks/useAuth";
 import { useRequests } from "@/hooks/useRequests";
+import { RequestCard } from "@/components/RequestCard";
 
 type StatCardProps = {
   icon: JSX.Element;
@@ -9,17 +10,9 @@ type StatCardProps = {
   label: string;
 };
 
-type RequestCardProps = {
-  bloodType: string;
-  urgency: string;
-  title: string;
-  location: string;
-  timePosted: string;
-};
-
 export default function HomeScreen() {
   const { userData } = useAuth();
-const {requests} = useRequests()
+  const { requests } = useRequests();
 
   return (
     <ScrollView className="flex-1 bg-gray-100">
@@ -64,9 +57,10 @@ const {requests} = useRequests()
         </View>
 
         <ScrollView
-        showsVerticalScrollIndicator={false}
-          className="pl-5">
-          {requests.map((req,index) => (
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          className="p-5">
+          {requests.map((req, index) => (
             <RequestCard
               key={index}
               bloodType={req.bloodType}
@@ -87,28 +81,5 @@ const StatCard: React.FC<StatCardProps> = ({ icon, number, label }) => (
     {icon}
     <Text className="mt-2 text-lg font-bold text-gray-800">{number}</Text>
     <Text className="mt-1 text-xs text-gray-600">{label}</Text>
-  </View>
-);
-
-const RequestCard: React.FC<RequestCardProps> = ({
-  bloodType,
-  urgency,
-  title,
-  location,
-  timePosted,
-}) => (
-  <View className="bg-white rounded-xl p-6  shadow-md">
-    <View className="flex-row justify-between mb-2">
-      <View className="bg-blue-100 px-3 py-1 rounded-md">
-        <Text className="text-xs font-semibold text-red-500">{bloodType}</Text>
-      </View>
-      <Text className="text-xs font-semibold text-red-500">{urgency}</Text>
-    </View>
-    <Text className="text-base font-semibold text-gray-800 mb-1">{title}</Text>
-    <Text className="text-sm text-gray-600 mb-1">{location}</Text>
-    <Text className="text-xs text-gray-500 mb-3">{timePosted}</Text>
-    <TouchableOpacity className="bg-primary rounded-md py-2 items-center">
-      <Text className="text-white text-sm font-medium">View Details</Text>
-    </TouchableOpacity>
   </View>
 );
---
+++ components/RequestCard.tsx
@@ -0,0 +1,32 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+type RequestCardProps = {
+  bloodType: string;
+  urgency: string;
+  title: string;
+  location: string;
+  timePosted: string;
+};
+
+export const RequestCard: React.FC<RequestCardProps> = ({
+  bloodType,
+  urgency,
+  title,
+  location,
+  timePosted,
+}) => (
+  <View className="bg-white rounded-xl p-6 w-60 shadow-md mx-1">
+    <View className="flex-row justify-between mb-2">
+      <View className="bg-blue-100 px-3 py-1 rounded-md">
+        <Text className="text-xs font-semibold text-red-500">{bloodType}</Text>
+      </View>
+      <Text className="text-xs font-semibold text-red-500">{urgency}</Text>
+    </View>
+    <Text className="text-base font-semibold text-gray-800 mb-1">{title}</Text>
+    <Text className="text-sm text-gray-600 mb-1">{location}</Text>
+    <Text className="text-xs text-gray-500 mb-3">{timePosted}</Text>
+    <TouchableOpacity className="bg-primary rounded-md py-2 items-center">
+      <Text className="text-white text-sm font-medium">View Details</Text>
+    </TouchableOpacity>
+  </View>
+);
